f59413432e2f42e338a36835262a17ccffef4c6b
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2023-03-02 14:38:44 +0500

    Selling page added

---
+++ Seller.App/Pages/Selling.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Seller.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for Selling.xaml
+    /// </summary>
+    public partial class Selling : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public Selling()
+        {
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            clock.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            date.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void logout_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
