c8896dcb5be986dc4df1ede4bd8ef9650c3a2445
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2023-04-13 14:17:12 +0500

    aaa

--- POS_System/Controllers/WarehouseItemController.cs
+++ POS_System/Controllers/WarehouseItemController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class WarehouseItemController : ControllerBase
     {
         private readonly IWarehouseItemService _itemService;
